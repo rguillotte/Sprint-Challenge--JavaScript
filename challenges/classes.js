f2953f92def29f642dd63159d387b659711dd6af
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-06-02 23:47:41 -0400

    finished README.md questions

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker{
+class CuboidMakerNew{
     constructor(attributes){
     this.length = attributes.length;
     this.width = attributes.width;
@@ -18,7 +18,7 @@ class CuboidMaker{
   }
 
 
-  const cuboid = new CuboidMaker({
+  const cuboidNew = new CuboidMakerNew({
     length: 4,
     width: 5,
     height: 5
@@ -27,7 +27,7 @@ class CuboidMaker{
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboidNew.volume()); // 100
+console.log(cuboidNew.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -105,14 +105,15 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-// const uni = [function(){
-//   return universities.includes('uni')
-// }];
+const uni = [function(){
+  for (let i = 0; i < universities.length - 1; i++){
+    if(universities[i].includes('uni')){
+      return universities[i];
+    };
+  }
+}]
 
-
-
-
-// console.log(uni);
+console.log(uni);
 
 
 // ==== ADVANCED Array Methods ====
